e992f5e48b29df0406a4a7a0fd6cab8eff96761b
artdznr <susanmcleannangle@gmail.com>
2026-07-19 17:13:00 -0400

    Refactor vite.config.ts by removing loadEnv
    Removed loadEnv import and its usage.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-
+import { defineConfig } from 'vite';
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
         resolve: {
